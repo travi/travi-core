5dbb21986f332fc814e3ab45cde284287b09c5c4
Matt Travi <programmer@travi.org>
2008-07-23 02:02:39 +0000

    Started update for radio buttons, still need slight adjustments. Also update for checkboxes needed.

--- client/js/jquery/plugins/formAlign/jquery.formAlign.js
+++ client/js/jquery/plugins/formAlign/jquery.formAlign.js
@@ -1,7 +1,7 @@
 jQuery.fn.alignFields = function() {
 	return this.each(function(){
 		if($.browser.mozilla){
-			$(this).find("ul.fieldList label, ul.fieldList .formBlock").each( function(i){
+			$(this).find("ul.fieldList li>label").each( function(i){
 				var labelSpan = $('<span>').css('display','block')
 					.width($(this).width()).html($(this).html());
 				$(this).empty().append(labelSpan).css('display','-moz-inline-box');
