0d6b40175abeffefc18a18241cc99114e49fe146
Matt Travi <programmer@travi.org>
2009-12-19 18:26:29 +0000

    Added utilities js file

---
+++ client/js/utilities.js
@@ -0,0 +1,20 @@
+/*!
+	JavaScript: The Good Parts by Douglas Crockford.
+	Copyright 2008 Yahoo! Inc., 978-0-596-51774-8
+ */
+if (typeof Object.create !== 'function'){
+	Object.create = function (o) {
+		var F = function () {};
+		F.prototype = o;
+		return new F();
+	};
+}
+
+/*!
+ Array Remove - By John Resig (MIT Licensed)
+  */
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
