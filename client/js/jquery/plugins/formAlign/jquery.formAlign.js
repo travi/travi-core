3931d6c621ba3a613361fc4799caff9d90ea5828
Matt Travi <programmer@travi.org>
2010-01-10 15:14:05 +0000

    new build file for framework. also added jslint task and cleansed in house js

--- client/js/jquery/plugins/formAlign/jquery.formAlign.js
+++ client/js/jquery/plugins/formAlign/jquery.formAlign.js
@@ -1,11 +1,13 @@
-jQuery.fn.alignFields = function() {
-	return this.each(function(){
-		if(($.browser.mozilla) && (parseFloat($.browser.version) < 1.9)){
-			$(this).find("ul.fieldList li>label").each( function(i){
-				var labelSpan = $('<span>').css('display','block')
+"use strict";
+
+jQuery.fn.alignFields = function () {
+	return this.each(function () {
+		if (($.browser.mozilla) && (parseFloat($.browser.version) < 1.9)) {
+			$(this).find("ul.fieldList li>label").each(function (i) {
+				var labelSpan = $('<span>').css('display', 'block')
 					.width($(this).width()).html($(this).html());
-				$(this).empty().append(labelSpan).css('display','-moz-inline-box');
-			} );
+				$(this).empty().append(labelSpan).css('display', '-moz-inline-box');
+			});
 		}
 	});
 };
--- client/js/utilities.js
+++ client/js/utilities.js
@@ -1,8 +1,10 @@
+"use strict";
+
 /*!
 	JavaScript: The Good Parts by Douglas Crockford.
 	Copyright 2008 Yahoo! Inc., 978-0-596-51774-8
  */
-if (typeof Object.create !== 'function'){
+if (typeof Object.create !== 'function') {
 	Object.create = function (o) {
 		var F = function () {};
 		F.prototype = o;
@@ -13,8 +15,8 @@ if (typeof Object.create !== 'function'){
 /*!
  Array Remove - By John Resig (MIT Licensed)
   */
-Array.prototype.remove = function(from, to) {
-  var rest = this.slice((to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
+Array.prototype.remove = function (from, to) {
+	var rest = this.slice((to || from) + 1 || this.length);
+	this.length = from < 0 ? this.length + from : from;
+	return this.push.apply(this, rest);
 };
