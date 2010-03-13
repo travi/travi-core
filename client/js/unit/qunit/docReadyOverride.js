e9c61264fe62a592e3da9334f5d31700f10e17c7
Matt Travi <programmer@travi.org>
2010-03-13 20:47:04 +0000

    ACCEPTED - # 34: JS Test Coverage http://travi.org/trac/ticket/34
    Clean up of ready function override

--- client/js/unit/qunit/docReadyOverride.js
+++ client/js/unit/qunit/docReadyOverride.js
@@ -1,8 +1,6 @@
 "use strict";
 
 jQuery.ready = function () {
-		// Make sure that the DOM is not already loaded
-//		if ( !jQuery.isReady ) {
 			// Remember that the DOM is ready
 			jQuery.isReady = true;
 
@@ -12,12 +10,8 @@ jQuery.ready = function () {
 				jQuery.each(jQuery.readyList, function () {
 					this.call(document, jQuery);
 				});
-
-				// Reset the list of functions
-//				jQuery.readyList = null;
 			}
 
 			// Trigger any bound ready events
 			jQuery(document).triggerHandler("ready");
-//		}
 	    };
