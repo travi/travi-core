b9d04b17ae3db81ef2b37c2499880dde47aad2ad
Matt Travi <programmer@travi.org>
2011-03-20 17:28:49 -0500

    fixed bug introduced by incorrect update of "use strict" rule

--- client/js/list/entityList.js
+++ client/js/list/entityList.js
@@ -1,79 +1,76 @@
-(function () {
+if (typeof travi !== 'object') {
+	var travi = {};
+}
+if (typeof travi.framework !== 'object') {
+    travi.framework = {};
+}
+
+travi.framework.entityList = (function () {
     "use strict";
+    
+    var  buttonText,
 
+    setMessage = function (confirmation) {
+        $("#confirmation").text(confirmation);
+    },
 
-    if (typeof travi !== 'object') {
-        document.travi = {};
-    }
-    if (typeof travi.framework !== 'object') {
-        travi.framework = {};
-    }
+    setText = function (text) {
+        this.buttonText = text;
+    },
 
-    travi.framework.entityList = (function () {
-        var  buttonText,
+    getText = function () {
+        return this.buttonText;
+    },
 
-        setMessage = function (confirmation) {
-            $("#confirmation").text(confirmation);
-        },
+    confirm = function () {
+        var $form = $(this);
 
-        setText = function (text) {
-            this.buttonText = text;
-        },
-
-        getText = function () {
-            return this.buttonText;
-        },
-
-        confirm = function () {
-            var $form = $(this);
-
-            $("#confirmation").dialog("option", "buttons", [
-                {
-                    text:   travi.framework.entityList.getButtonText(),
-                    click:  function () {
-                        $(this).dialog("close");
-                        $form.unbind("submit").submit();
-                    }
-                },
-                {
-                    text:   "Cancel",
-                    click:  function () {
-                        $(this).dialog("close");
-                    }
+        $("#confirmation").dialog("option", "buttons", [
+            {
+                text:   travi.framework.entityList.getButtonText(),
+                click:  function () {
+                    $(this).dialog("close");
+                    $form.unbind("submit").submit();
                 }
-            ]);
-            $("#confirmation").dialog("open");
+            },
+            {
+                text:   "Cancel",
+                click:  function () {
+                    $(this).dialog("close");
+                }
+            }
+        ]);
+        $("#confirmation").dialog("open");
 
+        return false;
+    },
+
+    init = function () {
+        $("li.remove-item form")
+            .hide()
+            .after("<a class='item-action' href='#'>Remove</a>");
+        $('ol.entityList').delegate('li.remove-item a.item-action', 'click', function () {
+            $(this).prev("form").submit();
             return false;
-        },
-
-        init = function () {
-            $("li.remove-item form")
-                .hide()
-                .after("<a class='item-action' href='#'>Remove</a>");
-            $('ol.entityList').delegate('li.remove-item a.item-action', 'click', function () {
-                $(this).prev("form").submit();
-                return false;
-            });
-            $("body").append("<div id='confirmation' title='Are you sure?'></div>");
-            $("#confirmation").dialog({
-                autoOpen:   false,
-                modal:      true,
-                resizable:  false
-            });
-            $("form.item-action").submit(confirm);
-            $('a.add-item').button({icons: {primary: 'ui-action-circle-plus'}});
-        };
-
-        $(document).ready(function () {
-            init();
         });
+        $("body").append("<div id='confirmation' title='Are you sure?'></div>");
+        $("#confirmation").dialog({
+            autoOpen:   false,
+            modal:      true,
+            resizable:  false
+        });
+        $("form.item-action").submit(confirm);
+        $('a.add-item').button({icons: {primary: 'ui-action-circle-plus'}});
+    };
 
-        return {
-            init                    : init,
-            setConfirmationMessage  : setMessage,
-            setButtonText           : setText,
-            getButtonText           : getText
-        };
-    }());
+    $(document).ready(function () {
+        init();
+    });
+
+    return {
+        init                    : init,
+        setConfirmationMessage  : setMessage,
+        setButtonText           : setText,
+        getButtonText           : getText
+    };
 }());
