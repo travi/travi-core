1384bd007834cfda8243924069688d25ded4c5b9
Matt Travi <programmer@travi.org>
2011-05-13 18:54:34 -0500

    now have local and remote loading options. defaulting to remote

--- client/js/qunit/bootstrap.js
+++ client/js/qunit/bootstrap.js
@@ -3,12 +3,16 @@ var $testEnvironmentLoader = $.Deferred();
 $.when(
 
     // Testing extensions
-    $.getScript('/resources/shared/js/qunit/qunit.js'),
-    $.getScript('/resources/shared/js/jquery/plugins/mockjax/jquery.mockjax.js'),
-    $.getScript('/resources/shared/js/jquery/plugins/template/jquery.tmpl.js'),
-//
-//    // ProjectX files
-//    $.getScript('js/projectx.js'),
+
+    //Local
+//    $.getScript('/resources/shared/js/qunit/qunit.js'),
+//    $.getScript('/resources/shared/js/jquery/plugins/mockjax/jquery.mockjax.js'),
+//    $.getScript('/resources/shared/js/jquery/plugins/template/jquery.tmpl.js'),
+
+    //Remote
+    $.getScript('http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js'),
+    $.getScript('http://code.appendto.com/plugins/jquery-mockjax/jquery.mockjax.js'),
+    $.getScript('http://code.jquery.com/qunit/git/qunit.js'),
 
     // DOM ready deferred object
     $.Deferred(function (deferred) {
@@ -21,8 +25,3 @@ $.when(
     $testEnvironmentLoader.resolve();
 
 });
-
-function remoteScripts() {
-    $.getScript('http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js');
-    $.getScript('http://code.jquery.com/qunit/git/qunit.js');
-}
