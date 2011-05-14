28d557673f3f3c8092caba875f197c0e9aaf9bec
Matt Travi <programmer@travi.org>
2011-05-14 11:16:44 -0500

    switched to using LABjs to load testrunner dependencies rather than jQuery deferreds

--- client/js/qunit/bootstrap.js
+++ client/js/qunit/bootstrap.js
@@ -1,27 +1,48 @@
-var $testEnvironmentLoader = $.Deferred();
+var $testEnvironmentLoader = null,
+    resourcesLocation = 'remote',
+    resources = {
+        jQuery: {
+            local: '/resources/shared/js/jquery/jquery.js',
+            remote: 'http://code.jquery.com/jquery-latest.js'
+        },
+        QUnit: {
+            local: '/resources/shared/js/qunit/qunit.js',
+            remote: 'http://code.jquery.com/qunit/git/qunit.js'
+        },
+        mockjax: {
+            local: '/resources/shared/js/jquery/plugins/mockjax/jquery.mockjax.js',
+            remote: 'http://code.appendto.com/plugins/jquery-mockjax/jquery.mockjax.js'
+        },
+        templates: {
+            local: '/resources/shared/js/jquery/plugins/template/jquery.tmpl.js',
+            remote: 'http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js'
+        },
+        travi: '/resources/shared/js/travi.js'
+    },
+    loadDependencies = function () {
+        $LAB
+            .script(resources.QUnit[resourcesLocation])
+            .script(resources.mockjax[resourcesLocation])
+            .script(resources.templates[resourcesLocation])
+            .script(resources.travi)
+            .wait(loadRunnerDependencies);
+    };
 
-$.when(
+loadDependencies();
 
-    // Testing extensions
+//$LAB
+//    .script(resources.jQuery.remote)
+//    .wait(function () {
+//        if (typeof window.jQuery === "undefined") {
+//           // first load failed, load local fallback, then dependencies
+//           $LAB.script(resources.jQuery.local).wait(loadDependencies);
+//        } else {
+//           // first load was a success, proceed to loading dependencies.
+//           loadDependencies();
+//        }
+//    });
 
-    //Local
-//    $.getScript('/resources/shared/js/qunit/qunit.js'),
-//    $.getScript('/resources/shared/js/jquery/plugins/mockjax/jquery.mockjax.js'),
-//    $.getScript('/resources/shared/js/jquery/plugins/template/jquery.tmpl.js'),
 
-    //Remote
-    $.getScript('http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js'),
-    $.getScript('http://code.appendto.com/plugins/jquery-mockjax/jquery.mockjax.js'),
-    $.getScript('http://code.jquery.com/qunit/git/qunit.js'),
 
-    // DOM ready deferred object
-    $.Deferred(function (deferred) {
-        $(deferred.resolve);
-    })
 
-).done(function () {
 
-    // Trigger any functions bound to this test environment loader
-    $testEnvironmentLoader.resolve();
-
-});
