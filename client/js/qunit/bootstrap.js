1b5fa3e78dc6e5b0ea90fa6837b0541955f1b7f5
Matt Travi <programmer@travi.org>
2011-05-14 12:52:49 -0500

    qunit bootstrap now loading jquery dynamically

--- client/js/qunit/bootstrap.js
+++ client/js/qunit/bootstrap.js
@@ -18,29 +18,17 @@ var $testEnvironmentLoader = null,
             remote: 'http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js'
         },
         travi: '/resources/shared/js/travi.js'
-    },
-    loadDependencies = function () {
-        $LAB
-            .script(resources.QUnit[resourcesLocation])
-            .script(resources.mockjax[resourcesLocation])
-            .script(resources.templates[resourcesLocation])
-            .script(resources.travi)
-            .wait(loadRunnerDependencies);
     };
 
-loadDependencies();
 
-//$LAB
-//    .script(resources.jQuery.remote)
-//    .wait(function () {
-//        if (typeof window.jQuery === "undefined") {
-//           // first load failed, load local fallback, then dependencies
-//           $LAB.script(resources.jQuery.local).wait(loadDependencies);
-//        } else {
-//           // first load was a success, proceed to loading dependencies.
-//           loadDependencies();
-//        }
-//    });
+$LAB
+    .script(resources.jQuery[resourcesLocation])
+    .wait()
+    .script(resources.QUnit[resourcesLocation])
+    .script(resources.mockjax[resourcesLocation])
+    .script(resources.templates[resourcesLocation])
+    .script(resources.travi)
+    .wait(loadRunnerDependencies);
 
 
 
