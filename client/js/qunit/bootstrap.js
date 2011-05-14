558be5c601ae9911ef61ac99806b92237ff2bd67
Matt Travi <programmer@travi.org>
2011-05-14 13:36:33 -0500

    design improvement

--- client/js/qunit/bootstrap.js
+++ client/js/qunit/bootstrap.js
@@ -1,41 +1,43 @@
 /*global loadRunnerDependencies*/
 
-var $testEnvironmentLoader = null,
-    resourcesLocation = 'remote',
-    resources = {
-        jQuery: {
-            local: '/resources/shared/js/jquery/jquery.js',
-            remote: 'http://code.jquery.com/jquery-latest.js'
-        },
-        QUnit: {
-            local: '/resources/shared/js/qunit/qunit.js',
-            remote: 'http://code.jquery.com/qunit/git/qunit.js'
-        },
-        QUnitStyle: {
-            local: '/resources/shared/js/qunit/qunit.css',
-            remote: 'http://code.jquery.com/qunit/git/qunit.css'
-        },
-        mockjax: {
-            local: '/resources/shared/js/jquery/plugins/mockjax/jquery.mockjax.js',
-            remote: 'http://code.appendto.com/plugins/jquery-mockjax/jquery.mockjax.js'
-        },
-        templates: {
-            local: '/resources/shared/js/jquery/plugins/template/jquery.tmpl.js',
-            remote: 'http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js'
-        },
-        travi: '/resources/shared/js/travi.js'
-    };
+(function () {
+    "use strict";
 
-$LAB
-    .script(resources.jQuery[resourcesLocation])
-    .wait()
-    .script(resources.QUnit[resourcesLocation])
-    .script(resources.mockjax[resourcesLocation])
-    .script(resources.templates[resourcesLocation])
-    .script(resources.travi)
-    .wait(function () {
-        "use strict";
+    var $testEnvironmentLoader = null,
+        resourcesLocation = 'remote',
+        resources = {
+            jQuery: {
+                local: '/resources/shared/js/jquery/jquery.js',
+                remote: 'http://code.jquery.com/jquery-latest.js'
+            },
+            QUnit: {
+                local: '/resources/shared/js/qunit/qunit.js',
+                remote: 'http://code.jquery.com/qunit/git/qunit.js'
+            },
+            QUnitStyle: {
+                local: '/resources/shared/js/qunit/qunit.css',
+                remote: 'http://code.jquery.com/qunit/git/qunit.css'
+            },
+            mockjax: {
+                local: '/resources/shared/js/jquery/plugins/mockjax/jquery.mockjax.js',
+                remote: 'http://code.appendto.com/plugins/jquery-mockjax/jquery.mockjax.js'
+            },
+            templates: {
+                local: '/resources/shared/js/jquery/plugins/template/jquery.tmpl.js',
+                remote: 'http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js'
+            },
+            travi: '/resources/shared/js/travi.js'
+        };
 
-        travi.getStyleSheet(resources.QUnitStyle[resourcesLocation]);
-        loadRunnerDependencies();
-    });
+    $LAB
+        .script(resources.jQuery[resourcesLocation])
+        .wait()
+        .script(resources.QUnit[resourcesLocation])
+        .script(resources.mockjax[resourcesLocation])
+        .script(resources.templates[resourcesLocation])
+        .script(resources.travi)
+        .wait(function () {
+            travi.getStyleSheet(resources.QUnitStyle[resourcesLocation]);
+            loadRunnerDependencies();
+        });
+}());
