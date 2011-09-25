9f6a0d76e79ea92b3818cc43eeb7018ab2d3250f
Matt Travi <programmer@travi.org>
2011-09-25 15:58:55 -0500

    tests for enhancement version cookie setting. also includes mocking ability for js tests

--- client/js/utilities.js
+++ client/js/utilities.js
@@ -1,11 +1,11 @@
-(function () {
+travi.framework.utils = (function () {
     "use strict";
-
 
     /*!
         JavaScript: The Good Parts by Douglas Crockford.
         Copyright 2008 Yahoo! Inc., 978-0-596-51774-8
      */
+
     if (typeof Object.create !== 'function') {
         Object.create = function (o) {
             var F = function () {};
@@ -22,5 +22,24 @@
         this.length = from < 0 ? this.length + from : from;
         return this.push.apply(this, rest);
     };
+
+    var extend = function (objectToExtend, additionalMembers) {
+            $.extend(objectToExtend, additionalMembers);
+        },
+
+        create = function (prototype, additionalMembers) {
+            var object = Object.create(prototype);
+
+            if (typeof additionalMembers === 'object') {
+                extend(object, additionalMembers);
+            }
+
+            return object;
+        };
+
+    return {
+        createObjectFrom: create,
+        extendObject    : extend
+    }
 
 }());
