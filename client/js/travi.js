28ab8a7d0acc237d5eaf41325628dde234912833
Matt Travi <programmer@travi.org>
2012-01-03 18:09:20 -0600

    switching back for now

--- client/js/travi.js
+++ client/js/travi.js
@@ -1,62 +1,62 @@
 var travi = (function () {
     "use strict";
 
-    function loadTemplate(templateUrl, templateName) {
-        $.get(templateUrl, function (template) {
-            $.template(templateName, template);
-        });
-    }
+    var loadTemplate = function (templateUrl, templateName) {
+            $.get(templateUrl, function (template) {
+                $.template(templateName, template);
+            });
+        },
 
-    function getStyleSheet(sheetUrl) {
-        var link = document.createElement("link");
+        getStyleSheet = function (sheetUrl) {
+            var link = document.createElement("link");
 
-        link.setAttribute("rel", "stylesheet");
-        link.setAttribute("type", "text/css");
-        link.setAttribute("href", sheetUrl);
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute("type", "text/css");
+            link.setAttribute("href", sheetUrl);
 
-        document.getElementsByTagName("head")[0].appendChild(link);
-    }
+            document.getElementsByTagName("head")[0].appendChild(link);
+        },
 
-    function namespace(ns, provided) {
-        var object = this,
-            levels = ns.split("."),
-            levelCount = levels.length,
-            i;
+        namespace = function (ns, provided) {
+            var object = this,
+                levels = ns.split("."),
+                levelCount = levels.length,
+                i;
 
-        for (i = 0; i < levelCount; i += 1) {
-            if (typeof object[levels[i]] === "undefined") {
-                if (i === levelCount - 1) {
-                    object[levels[i]] = provided || {};
-                } else {
-                    object[levels[i]] = {};
+            for (i = 0; i < levelCount; i += 1) {
+                if (typeof object[levels[i]] === "undefined") {
+                    if (i === levelCount - 1) {
+                        object[levels[i]] = provided || {};
+                    } else {
+                        object[levels[i]] = {};
+                    }
+                }
+
+                object = object[levels[i]];
+            }
+
+            return object;
+        },
+
+        constants = (function () {
+            var ownProp = Object.prototype.hasOwnProperty,
+                constants = {};
+
+            function set(key, value) {
+                if (!ownProp.call(constants, key)) {
+                    constants[key] = value;
                 }
             }
 
-            object = object[levels[i]];
-        }
-
-        return object;
-    }
-
-    var constants = (function () {
-        var ownProp = Object.prototype.hasOwnProperty,
-            constants = {};
-
-        function set(key, value) {
-            if (!ownProp.call(constants, key)) {
-                constants[key] = value;
+            function get(key) {
+                return constants[key];
             }
-        }
 
-        function get(key) {
-            return constants[key];
-        }
-
-        return {
-            set: set,
-            get: get
-        };
-    }());
+            return {
+                set: set,
+                get: get
+            };
+        }());
 
     return {
         loadTemplate    : loadTemplate,
