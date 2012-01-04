8ab3ea89ee83aff7647995270d589c8a8ce796c9
Matt Travi <programmer@travi.org>
2012-01-04 07:39:14 -0600

    putting deferred logic back in in expectation of using it to wait for template to load b4 using it

--- client/js/travi.js
+++ client/js/travi.js
@@ -2,9 +2,14 @@ var travi = (function () {
     "use strict";
 
     var loadTemplate = function (templateUrl, templateName) {
+            var deferred = $.Deferred();
+
             $.get(templateUrl, function (template) {
                 $.template(templateName, template);
+                deferred.resolve();
             });
+
+            return deferred.promise();
         },
 
         getStyleSheet = function (sheetUrl) {
