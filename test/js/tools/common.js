93057475ecaf82a09d13727789c1bd14fb13d006
Matt Travi <programmer@travi.org>
2012-07-14 09:00:37 -0500

    clean up

--- test/js/tools/common.js
+++ test/js/tools/common.js
@@ -5,5 +5,16 @@ travi.test.common = {
         if (functionName.restore) {
             functionName.restore();
         }
+    },
+
+    stubGetTemplates: function () {
+        sinon.stub(travi.templates, 'get', function () {
+            var deferred = new $.Deferred(),
+                promise = deferred.promise();
+
+            deferred.resolve();
+
+            return promise;
+        });
     }
 };
