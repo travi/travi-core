1a0ddce5d67b90eb1234501625400dbf40209893
Matt Travi <programmer@travi.org>
2013-08-24 19:31:17 -0500

    got tests working again

---
+++ test/resources/load-templates.js
@@ -0,0 +1,7 @@
+travi.templates.init();
+
+travi.templates.preLoad('chooseEnhancement', '/base/templates/enhancementVersion.tmpl');
+
+travi.templates.preLoad('update-item', '/base/test/templates/stub-update-item.tmpl');
+travi.templates.preLoad('entityList', '/base/test/templates/entity-list.tmpl');
+travi.templates.preLoad('pagination', '/base/test/templates/pagination.tmpl');
