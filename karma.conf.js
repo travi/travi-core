1cada8a06546b349e9478b142acd6bc8bfeca790
Matt Travi <programmer@travi.org>
2014-02-09 14:06:48 -0600

    separate grunt task for ui namespace

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function (config) {
             'test/resources/load-templates.js',
             'js/travi/location.js',
             'js/travi/events.js',
+            'js/travi/dependencies/*.js',
             'js/travi/ui/pagination.js',
             'bower_components/travi-test-utils/test-init.js',
             'js/**/*.js',
