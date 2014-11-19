45948a860e06e94188b5ed03571d8447a4157baf
Matt Travi <programmer@travi.org>
2014-11-19 13:57:04 -0800

    added tests for what will be the build of the critical.js file

---
+++ critical.conf.js
@@ -0,0 +1,33 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ['referee'],
+
+        files: [
+            {pattern: 'templates/**/*.tmpl', included: false},
+
+            'bower_components/travi-test-utils/tools/assert-cache.js',
+            'node_modules/karma-jstd-adapter/jstd-adapter.js',
+            'bower_components/travi-test-utils/tools/assert-fix.js',
+
+            'bower_components/travi-test-utils/stubs/modernizrStub.js',
+
+            'js/travi.js',
+            'bower_components/travi-test-utils/tools/*.js',
+            'js/travi/browserProxy.js',
+            'js/travi/cookies.js',
+            'js/travi/enhancements/constants.js',
+            'js/travi/enhancements/persist.js',
+            'js/travi/enhancements/detection.js',
+            'bower_components/travi-test-utils/test-init.js',
+
+            'test/cookies.jstd',
+            'test/enhancements/screenSize.jstd'
+        ],
+
+        exclude: [
+            'bower_components/travi-test-utils/tools/cucumber-world.js'
+        ],
+
+        browsers: ['PhantomJS']
+    });
+};
